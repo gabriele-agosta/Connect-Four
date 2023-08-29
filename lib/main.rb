6bd9f7d88ccdf7ca56b946a889ded04cac1b5efd
g.agosta02 <g.agosta02@gmail.com>
2023-08-29 18:21:35 +0200

    Typo

--- lib/main.rb
+++ lib/main.rb
@@ -10,10 +10,7 @@ until game.game_over?(player)
   column = 0
   puts "It's your turn Player #{player}!"
   puts "Select in which column to insert your token"
-
   column = gets.to_i until column.between?(1, 7)
-
   game.insert_token(column - 1, player)
 end
-
 
